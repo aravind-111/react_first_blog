f8a276963233eda4e53974ef8dff0934f1591279
Aravindan-111 <aravindhan11195@gmail.com>
2021-09-16 16:26:55 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+import Home from './Pages/Home';
+import About from './Pages/About';
+import Posts from './Pages/Posts';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/About" component={About} />
+        <Route path="/Posts" component={Posts} />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
---
+++ src/Pages/Posts.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Posts() {
+  return <h1>Posts !!</h1>;
+}
+
+export default Posts;
