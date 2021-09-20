73532901bc4c43c81599e982e479eade7b38c790
Aravindan-111 <aravindhan11195@gmail.com>
2021-09-20 06:49:24 +0530

    comments added

--- src/Component/comments.js
+++ src/Component/comments.js
@@ -1,24 +1,39 @@
+import { useState, useEffect } from 'react';
 import React from 'react';
 import axios from 'axios';
 
 function Comments({ match }) {
   let b = match.params.id;
   console.log(b);
-  //   const [post, setPost] = useState({});
-  try {
-    let fetchData = async () => {
-      const comments = await axios.get(
-        `https://jsonplaceholder.typicode.com/posts/${b}/comments`
-      );
-      console.log(comments.data);
-    };
+  console.log('hello');
+  const [post, setPost] = useState([]);
+
+  let fetchData = async () => {
+    const { data } = await axios.get(
+      `https://jsonplaceholder.typicode.com/posts/${b}/comments`
+    );
+    console.log(data);
+    setPost(data);
+  };
+
+  useEffect(() => {
     fetchData();
-  } catch (err) {
-    console.log('error in comments');
-  }
+  }, []);
+
   return (
     <>
-      <p>poda</p>
+      {/* <p>{post.data[0].body}</p> */}
+      {post.map((data) => {
+        return (
+          <div className="bigWraper" key={data.id}>
+            <p>ID: {data.id}</p>
+            <p>Name : {data.name}</p>
+            <p>Email : {data.email}</p>
+            <p>Body : {data.body}</p>
+            <br />
+          </div>
+        );
+      })}
     </>
   );
 }
